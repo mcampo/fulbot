bb00c7203d75ebd098283eda8f393e1b773260e7
mcampo <mcampo85@gmail.com>
2013-06-14 11:49:24 -0300

    set mandrill_events as request param

--- fulbot-web/src/main/java/fulbot/web/IncomingEmailController.java
+++ fulbot-web/src/main/java/fulbot/web/IncomingEmailController.java
@@ -1,14 +1,17 @@
 package fulbot.web;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
@@ -22,9 +25,9 @@ public class IncomingEmailController {
 	public void head() {
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	@ResponseStatus(HttpStatus.OK)
-	public void processInboundMessage(@RequestBody HashMap json) {
+	public void processInboundMessage(@RequestParam("mandrill_events") List<HashMap> json) {
 		logger.info(json.toString());
 	}
 	
