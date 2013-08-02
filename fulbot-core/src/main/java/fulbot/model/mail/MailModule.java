e6e359d629453131d221fe5698e670d30dc03619
mcampo <mcampo85@gmail.com>
2013-08-02 11:31:15 -0300

    use spring mail sender

--- fulbot-core/src/main/java/fulbot/model/mail/MailModule.java
+++ fulbot-core/src/main/java/fulbot/model/mail/MailModule.java
@@ -1,9 +1,10 @@
 package fulbot.model.mail;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-//@Import(com.github.jknack.mwa.mail.MailModule.class)
+@Import(com.github.jknack.mwa.mail.MailModule.class)
 public class MailModule {
 
 }
